75ba7899b211d54de0dd8b0ded1efbd022895951
Angelica Glean <gllean23ange@gmail.com>
2021-05-28 16:50:21 +0800

    logapp-glean

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	//define('ROOT_URL', 'sql6.freesqldatabase.com');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6415433');
+	define('DB_PASS', 'i6ivvxBejE');
+	define('DB_NAME', 'sql6415433');
---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/Kbacaser28">Kbacaser28</a> for Assigment in BSCS 3B1</p>
+    </div>
+</footer>
+  </body>
+</html> 
